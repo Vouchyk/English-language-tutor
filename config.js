7d94f9c2bd95139b71e0f4d6829bbed45bc56b5b
Vouchyk <151055502+Vouchyk@users.noreply.github.com>
2024-10-30 11:47:54 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// config.js
-window.config = {
-  BOT_TOKEN: '8194466205:AAGJ48FK9d3OMJ07J3HHDYdk28lUgBIoX_A', // Замініть на свій токен
-  CHAT_ID: '-1002259497759' // Замініть на ID вашого чату
-};
